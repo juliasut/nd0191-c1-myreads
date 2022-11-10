26f273661bb02ca726c2de308cea90448d114528
juliasut <juliasutygina@gmail.com>
2022-11-10 00:05:36 -0700

    feat: Display books filtered by shelf

--- starter/src/components/BookShelves.js
+++ starter/src/components/BookShelves.js
@@ -4,9 +4,9 @@ import BookShelf from './BookShelf';
 export default function BookShelves({books}) {
   return (
     <>
-      <BookShelf title="Currently Reading" books={books}/>
-      <BookShelf title="Want To Read" books={books}/>
-      <BookShelf title="Read" books={books}/>
+      <BookShelf title="Currently Reading" books={books.filter((book) => book.shelf === "currentlyReading")} />
+      <BookShelf title="Want To Read" books={books.filter((book) => book.shelf === "wantToRead")} />
+      <BookShelf title="Read" books={books.filter((book) => book.shelf === "read")}/>
     </>
   );
 }
